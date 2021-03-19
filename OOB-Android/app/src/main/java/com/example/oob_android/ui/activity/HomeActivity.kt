6f27cc2031483c9eb577985c89dd8f91c83359c3
minhee <jwmh15@gmail.com>
2021-03-19 21:54:36 +0900

    Create HomeActivity

---
+++ OOB-Android/app/src/main/java/com/example/oob_android/ui/activity/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.oob_android.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.oob_android.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
